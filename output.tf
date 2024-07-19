899dbc8a381b0b083ef84f29cf32ec497d7fe7bc
VJohnson1 <vjohnson@williamhill.us>
2024-07-19 09:24:46 -0700

    updated vm image

--- output.tf
+++ output.tf
@@ -5,11 +5,3 @@ output "resource_group_name" {
 output "resource_group_location" {
   value = azurerm_resource_group.main.location
 }
-
-output "vm" {
-  value = azurerm_linux_virtual_machine.linux.public_ip_address
-}
-
-output "vm_ami" {
-  value = azurerm_linux_virtual_machine.linux.source_image_id
-}
--- vm.tf
+++ vm.tf
@@ -1,9 +1,8 @@
 resource "azurerm_linux_virtual_machine" "linux" {
   depends_on = [azurerm_ssh_public_key.default]
-
   admin_username        = var.default_username
   location              = var.resource_group_location
-  name                  = "firstlinuxvm"
+  name                  = "linuxvm"
   network_interface_ids = [azurerm_network_interface.default.id]
   resource_group_name   = azurerm_resource_group.main.name
   size                  = "Standard_B1s"
@@ -16,12 +15,13 @@ resource "azurerm_linux_virtual_machine" "linux" {
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
+    disk_size_gb         = 10
   }
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "ubuntu-24_04-lts"
-    sku       = "24_04-lts"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
     version   = "latest"
   }
 }
