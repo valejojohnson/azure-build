c9d6209d82ece04be8e63eb26ae270053bf264d4
VJohnson1 <vjohnson@williamhill.us>
2024-08-20 15:31:16 -0700

    adding notes in vars.tf

--- vars.tf
+++ vars.tf
@@ -6,12 +6,12 @@ variable "resource_group_location" {
 
 variable "resource_group_name_prefix" {
   type        = string
-  default     = "TF"
+  default     = "TF" # Meaning Terraformed
   description = "Prefix for the name of the default resource group."
 }
 
 variable "default_username" {
   type        = string
   default     = "masteruser"
-  description = "Master Username"
+  description = "The master username to be used on the VM"
 }
